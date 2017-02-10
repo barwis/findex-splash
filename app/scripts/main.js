1fc500c7e47e9e97917440a0d063a76e139b0742
barwis <lordzix@gmail.com>
2017-02-10 16:19:49 +0000

    responsive popup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,7 @@ window.SignupForm.prototype = {
 	originalSize: {},
 	size: {},
 	padding: .1,
+	borderRadius: 0,
 	snap: null,
 
 	init: function() {
@@ -37,6 +38,8 @@ window.SignupForm.prototype = {
 			width: this.originalSize.width * .8,
 			height: this.originalSize.height * .8
 		};
+		this.borderRadius = Math.min(this.originalSize.width, this.originalSize.height) / 100;
+
 	},
 	getSize: function() {
 		return this.size;
@@ -46,105 +49,88 @@ window.SignupForm.prototype = {
 		this.snap = Snap('#' + this.svgRef);
 
 		// add first circle
-		var c0x = this.size.width * .58,
-			c0y = this.size.height * .5,
-			c0r = this.size.width * .414,
-			c1x = this.size.width * .174,
-			c1y = this.size.height * .215,
-			c1r = c1x,
-			c2x = this.size.width * .392,
-			c2y = this.size.height * .095,
-			c2r = this.size.width * .09,
-			c3x = this.size.width * .607,
-			c3y = this.size.height * .91,
-			c3r = this.size.width * .085,
-			c4x = this.size.width * .8,
-			c4y = this.size.height * .863,
-			c4r = this.size.width * .115,
-			c5x = c1x,
-			c5y = c1y, 
-			c5r = c1r - 10,
+		var centerX = this.size.width / 2,
+			centerY = this.size.height / 2,
+			borderRadius = this.borderRadius,
+			minCenter = Math.min(centerX, centerY),
+			c0x = centerX + minCenter * 0.1734197731,
+			c0y = centerY, 
+			c0r = minCenter * 0.8265802269,
+			c1x = centerX - minCenter * 0.2000850746,
+			c1y = centerY - minCenter * 0.7817504052,
+			c1r = minCenter * 0.1641791045,
+			c2x = centerX - minCenter * 0.6531604538,
+			c2y = centerY - minCenter * 0.5316045381,
+			c2r = minCenter * 0.3468395462,
+			c3x = centerX + minCenter * 0.2126623377,
+			c3y = centerY + minCenter * 0.7795786062,
+			c3r = minCenter * 0.1588330632,
+			c4x = centerX + minCenter * 0.5867098865,
+			c4y = centerY + minCenter * 0.6969205835,
+			c4r = minCenter * 0.2188006483,
+			imgMask = this.snap.circle(c2x, c2y, 0).attr({'fill': 'white'}),
 			circles = [
-				this.snap.circle(c0x, c0y, 0).attr('id', 'circle0'), 																//0
-				this.snap.circle(c2x, c2y + c2r, 0).attr('id', 'circle2'),															//1
-				this.snap.circle(c1x, c1y, 0).attr({'fill': '#ed5153', 'id': 'circle1', 'stroke': '#ffffff', 'stroke-width' : 20}), //2
-				this.snap.circle(c3x, c3y, 0).attr('id', 'circle3'),																//3
-				this.snap.circle(c4x, c4y, 0).attr('id', 'circle4')																	//4
-			],
-			mask = this.snap.circle(c1x, c1y, 0),
-			maskG = this.snap.group(mask),
-			image = this.snap.image('/images/pop_img.png'),
-			imageWidth = 564,
-			scale = imageWidth / c1r /5,
+				this.snap.circle(c0x, c0y, 0).attr({'id': 'circle0', 'fill': '#d8eaf6'}),
+				this.snap.group(
+					this.snap.circle(c1x, c1y, c1r).attr({'fill': 'none', 'stroke': '#d8eaf6', 'stroke-width': this.borderRadius}),
+					this.snap.image('/images/logo_full.svg', (c1x - c1r + this.borderRadius / 4), (c1y - c1r + this.borderRadius /4), c1r * 2 - this.borderRadius / 2, c1r * 2 - this.borderRadius /2)
+				).attr('id', 'circle1').transform('s0,0'),
+				this.snap.group(
+					this.snap.circle(c2x, c2y, 0).attr({'fill': '#ed5153', 'stroke': '#d8eaf6', 'stroke-width': this.borderRadius}),
+					this.snap.image('/images/pop_img.png', (c2x - c2r + this.borderRadius/2), (c2y - c2r + this.borderRadius/2), c2r * 2 -  this.borderRadius, c2r * 2 - this.borderRadius).attr({mask: imgMask})
+				).attr('id', 'circle2'),//.transform('s0,0'),
+				this.snap.group(
+					this.snap.circle(c3x, c3y, c3r).attr({'fill': '#ef4721', 'stroke': '#d8eaf6', 'stroke-width': this.borderRadius}),
+					this.snap.text(c3x, c3y, 'Close').attr({'text-anchor': 'middle', 'font-size': minCenter/100 + 'rem', 'dominant-baseline' : 'central'})
+				).transform('s0,0'),
+				this.snap.group(
+					this.snap.circle(c4x, c4y, c4r).attr({'fill': '#d5d9db', 'stroke': '#d8eaf6', 'stroke-width': this.borderRadius}),
+					this.snap.text(c4x, c4y, 'Send').attr({'text-anchor': 'middle', 'font-size': minCenter/80 +'4rem', 'dominant-baseline' : 'central'})
 
-			imageGroup = this.snap.group(image).attr({mask: maskG});//
+				).transform('s0,0')
+			];
 
-			image.transform(new Snap.Matrix().scale(scale, scale));
-
-
-				// this.snap.circle(c5x, c5y, 0).attr({'fill': '#ed5153', 'id': 'circle5'})
-
-		// var t = new Snap.Matrix().translate(this.originalSize.width * this.padding, this.originalSize.height * this.padding);
-
-		this.maskGroup = this
-							.snap
-							.group(circles[0], circles[1], circles[2], circles[3], circles[4], imageGroup)
-							.transform(new Snap.Matrix().translate(this.originalSize.width * this.padding, this.originalSize.height * this.padding))
-							.attr('id', 'myGroup')
-							.attr('filter', 'url(#dropshadow)');
-
-
-		mask.animate({
-			r: c1r
-		}, 2000, mina.backout);
+		this
+			.snap
+			.group(circles[0], circles[1], circles[2], circles[3], circles[4])//, circles[3], circles[4])
+			.transform(new Snap.Matrix().translate(this.originalSize.width * this.padding, this.originalSize.height * this.padding))
+			.attr('id', 'myGroup')
+			.attr('filter', 'url(#black-glow)');
 
 		var animations = [
 			{r: c0r},
-			{r: c2r, cy: c2y},
-			{r: c1r, cx: c1x, cy: c1y},
-			{r: c3r, cy: c3y},
-			{r: c4r},
+			{transform:'s1,1'},
+			{r: c2r},
+			{transform:'s1,1'},
+			{transform:'s1,1'}
+
+			//{r: c1r},
+			//{transform:'s1,1'},
+			//{transform:'s1,1'}
 			// {r: c5r}
 		];
 
 		for (var i = 0; i < animations.length; i++) {
-			(function(index) {
-				var c = i;
-				window.setTimeout(function() {
-					circles[c].animate(animations[c], 250, mina.backout);
-				}, c * 150);
-			})(i);
 
+			if (i === 2) {
+				(function(index) {
+					var c = i;
+					window.setTimeout(function() {
+						circles[c][0].animate(animations[c], 250, mina.backout);
+						console.log(c2r, borderRadius);
+						imgMask.animate({r: c2r - borderRadius /2 }, 250, mina.backout);
+					}, c * 150);
+				})(i);
+
+			} else {
+				(function(index) {
+					var c = i;
+					window.setTimeout(function() {
+						circles[c].animate(animations[c], 250, mina.backout);
+					}, c * 150);
+				})(i);
+			}
 		}
-
-		// circle1.animate({r: c1r}, 500, mina.backout);
-
-		// window.setTimeout(function() {
-		// 	circle2.animate({
-		// 		r: c2r,
-		// 		cx: c2x,
-		// 		cy: c2y
-		// 	}, 500, mina.backout);
-		// }, 500);
-
-		// window.setTimeout(function() {
-		// 	circle3.animate({
-		// 		r: c3r, 
-		// 		cy: c3y
-		// 	}, 500, mina.backout);
-		// }, 1000)
-
-		// this.snap.use()
-		// 	.attr({
-		// 		'xlink:href': '#myGroup',
-		// 		'width': this.size.width,
-		// 		'height': this.size.height
-		// 	});
-
-
-		// return this.snap;
-
-
 	}
 
 };
